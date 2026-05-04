328baaac3726b455135722bee766b236bf051cd2
yatsuZ <yassine.zaoui28062003@gmail.com>
2026-05-04 13:50:05 +0200

    faire la classe piece et ameliore rle recorde etc poru que tout soit bien

--- code/src/server/class/Piece.ts
+++ code/src/server/class/Piece.ts
@@ -1,7 +1,26 @@
-import { describe, it, expect } from 'vitest'
+import type { I_Piece } from '../interface/I_Piece.js';
+import { Shapes } from './Shapes.js';
 
-describe('Piece', () => {
-  it('should work', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
+class Piece implements I_Piece {
+  readonly shape: I_Piece['shape'];
+  orientation: I_Piece['orientation'];
+  x: number;
+  y: number;
+
+  constructor(shape: I_Piece['shape'], x: number, y: number) {
+    this.shape = shape;
+    this.orientation = 0;
+    this.x = x;
+    this.y = y;
+  }
+
+  getMatrix(): number[][] {
+    return Shapes[this.shape][this.orientation] as number[][];
+    throw new Error("Method not implemented.");
+  }
+  rotate(): void {
+    this.orientation = (this.orientation + 1) % 4 as I_Piece['orientation'];
+    return;
+    throw new Error("Method not implemented.");
+  }
+}
--- code/src/server/interface/I_Piece.ts
+++ code/src/server/interface/I_Piece.ts
@@ -1,8 +1,12 @@
+export type PieceShape = 'I' | 'J' | 'L' | 'O' | 'S' | 'T' | 'Z';
+export type Matrix = number[][];
+export type Rotations = [Matrix, Matrix, Matrix, Matrix];
+
 export interface I_Piece {
-    readonly shape: 'I' | 'J' | 'L' | 'O' | 'S' | 'T' | 'Z';
+    readonly shape: PieceShape;
     orientation: 0 | 1 | 2 | 3;
     x: number;
     y: number;
-    getMatrix(): boolean[][];
+    getMatrix(): Matrix;
     rotate(): void;
 }
