45bcbb4ba79031d36b1ddc73e241558b003cada3
yatsuZ <yassine.zaoui28062003@gmail.com>
2026-05-04 10:18:33 +0200

    init de l'arboraissance server et init des test plus config

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -8,6 +8,18 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       port: parseInt(env.CORS_PORT || '5173')
+    },
+    test: {
+      coverage: {
+        provider: 'v8',
+        include: ['src/server/**/*.ts'],
+        thresholds: {
+          statements: 70, // 70% des instructions exécutées par les tests
+          functions: 70,  // 70% des fonctions appelées
+          lines: 70,      // 70% des lignes couvertes
+          branches: 50    // 50% des chemins if/else, ternaires, etc.
+        }
+      }
     }
   }
 })
