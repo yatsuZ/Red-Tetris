ba559382b63798e1cffbcbca139ca5ba7e388346
yatsuZ <yassine.zaoui28062003@gmail.com>
2026-05-11 12:24:20 +0200

    test clone fais

--- code/test/server/class/Piece.test.ts
+++ code/test/server/class/Piece.test.ts
@@ -5,25 +5,14 @@ import type { PieceData } from '../../../src/server/interface/I_Piece.js';
 import { test_Constructeur } from './Piece/constructeur.js';
 import { test_rotation } from './Piece/rotate.js';
 import { test_matrix } from './Piece/matrix.js';
+import { test_clone } from './Piece/clone.js';
 
 describe('Piece', () => {
-    describe('constructor', test_Constructeur)
-    describe('rotate', test_rotation)
-    describe('getMatrix', test_matrix)
+    describe('constructor', test_Constructeur);
+    describe('rotate', test_rotation);
+    describe('getMatrix', test_matrix);
+    describe('clone', test_clone);
       /*
-    describe('clone', () => { 
-      // Arrange
-      const piece = new Piece('I', { x: 5, y: 4 });
-
-      // Act — (ici rien, on teste juste l'état initial)
-      const clonePiece : Piece = piece.clone();
-      // Assert
-      expect(piece).toEqual(clonePiece);
-      expect(piece.orientation).toEqual(clonePiece.orientation);
-      clonePiece.rotate();
-      expect(piece.orientation).not.equal(clonePiece.orientation);
-
-     })
     describe('get', () => { 
       // Arrange
       const piece = new Piece('I', { x: 5, y: 4 });
---
+++ code/test/server/class/Piece/clone.ts
@@ -0,0 +1,52 @@
+import { expect, it } from "vitest";
+import { Piece } from "../../../../src/server/class/Piece.js";
+import { type I_Piece, type PieceData, type Position } from "../../../../src/server/interface/I_Piece.js";
+import { getRandomInt } from "../../../../src/server/utils/rng.js";
+import { ALL_SHAPES, PieceOrientationAffichage } from "../../../../src/server/constant/Shapes.js";
+
+function defaut({ shape, position, rotation}: { shape: I_Piece['shape'], position: Position, rotation : 0|1|2|3}) {
+    // Arrange
+    const piece = new Piece(shape, position, rotation);
+    const clonePiece : Piece = piece.clone();
+
+    // Assert
+    // Vérifie que ce ne sont pas les mêmes instances en mémoire
+    expect(piece).not.toBe(clonePiece);
+    expect(piece).toEqual(clonePiece);
+    expect(piece.orientation).toEqual(clonePiece.orientation);
+
+    // Act — 
+    clonePiece.rotate();
+
+    // Assert
+    expect(piece.orientation).not.equal(clonePiece.orientation);
+  }
+
+export function test_clone(): void
+{ 
+  const rng_rotation : 0|1|2|3 = getRandomInt(0, 3) % 4 as 0|1|2|3;
+  const rng_pos : Position = {x: getRandomInt(-100, 100), y:getRandomInt(-100, 100)};
+
+  const all_param: {
+    shape: I_Piece['shape'], 
+    position: Position, 
+    rotation: 0|1|2|3, 
+    arrow_init: string
+  }[] =
+  ALL_SHAPES.map(shape => 
+    (
+      { 
+        shape, 
+        position: rng_pos, 
+        rotation: rng_rotation, 
+        arrow_init: PieceOrientationAffichage[rng_rotation] 
+      }
+    )
+  );
+
+  it.each(all_param)
+  (
+    'Shape $shape en $arrow_init($position.x, $position.y) cloné, objet distinct.', 
+    defaut
+  );
+}
